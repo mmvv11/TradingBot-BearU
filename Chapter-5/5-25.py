dcb6cfe1671b05118b24d22c7d34ae679f7ef3c6
ronnie-colmen <ronnie@softsquared.com>
2021-07-16 13:02:33 +0900

    매매로직 구현중

--- Chapter-5/5-25.py
+++ Chapter-5/5-25.py
@@ -7,6 +7,7 @@ secret = "FZatuQ65in9k1rmd8DOIxmzAiLGAvxR6E1dwL3p5"
 upbit = pyupbit.Upbit(access, secret)
 pd.options.display.float_format = '{:.1f}'.format
 
+
 def isBullMarket(interval):
     df = pyupbit.get_ohlcv("KRW-BTC", interval=interval)
     period = 20  # 일이 아니라 갯수
@@ -45,13 +46,64 @@ plt.show()
 
 n초 간격으로 현재가 감시, 
 상승장 매수 구간, 상승장 매도구간 도달시 매매 이벤트 발생
-"""
-def bullTradingLogic():
-    """
-    상승장 매매 전략
-    """
 
-def bearTradingLogic():
-    """
-    하락장 매매 전략
-    """
+상승장 매수: 이전 봉 종가 20선 아래에 있을 때, 20선 +n% 범위에서 매수
+상승장 매도: 천장 +-n% 범위에서 매도
+"""
+
+from time import sleep
+
+
+class Bot():
+    def __init__(self):
+        """
+        20선, 천장, 바닥 가격을 속성으로 가지고 있어야함
+        +-n% 범위를 속성으로 가지고 있어야함
+        """
+        self.MA20 = 1
+        self.ceiling = 1
+        self.bottom = 1
+        self.tradingRange = 1
+        self.previousClosePrice = 1
+
+    def startBot(self):
+        while True:
+            currentPrice = pyupbit.get_current_price()
+            status = self.getStatus(currentPrice)
+            self.tradingLogic(status)
+            sleep(1)
+
+    def getStatus(self, price):
+        maxBuyingRange = self.MA20 * (1 + self.tradingRange)
+        minSellingRange = self.ceiling * (1 - self.tradingRange)
+
+        buyingCondition = (self.MA20 <= price <= maxBuyingRange) and (self.previousClosePrice < self.MA20)
+        sellingCondition = minSellingRange <= price
+
+        if buyingCondition:
+            return "buy"
+        if sellingCondition:
+            return "sell"
+        return None
+
+    def tradingLogic(self, status):
+        if not status:
+            return
+        if status == "buy":
+            """
+            풀매수
+            """
+        if status == "sell":
+            """
+            풀매도
+            """
+
+        # def bullTradingLogic():
+#     """
+#     상승장 매매 전략
+#     """
+#
+# def bearTradingLogic():
+#     """
+#     하락장 매매 전략
+#     """
